cba00f78fc6a1cdbba2a1bab36ae3d6869b4207a
OraLevchenko <40594549+OraLevchenko@users.noreply.github.com>
2018-06-26 10:51:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import Configuration
 
+# My microservice!
+
 # create Flask app
 app = Flask(__name__)
 app.config.from_object(Configuration)
